54588100cf159973b94625e78075a518d6cd0495
mdataurrahman555 <74402921+mdataurrahman555@users.noreply.github.com>
2022-02-28 23:44:48 +0600

    added cards and card add function with js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Global Variable
 const inputField = document.getElementById('input-field');
-
+const cardsSection = document.getElementById('cards-section');
 
 //---Search Function----///
 const search = () => {
@@ -8,5 +8,28 @@ const search = () => {
     inputField.value = '';
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayCards(data.data));
+}
+
+// ---Display Cards---- ///
+const displayCards = (data) => {
+    data.slice(0, 20).forEach(element => {
+        const div = document.createElement('div');
+        div.classList.add("col");
+        console.log(element);
+        div.innerHTML = 
+        `
+            <div class="card">
+                <div class='w-50 h-50 mx-auto py-3'>
+                    <img src="${element.image}" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Brand: ${element.brand}</h5>
+                    <p class="card-text">Phone Name: ${element.phone_name}</p>
+                    <button class="btn btn-secondary">Details</button>
+                </div>
+            </div>
+        `;
+        cardsSection.appendChild(div);
+    });
 }
